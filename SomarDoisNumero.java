e4e73805cd6fe8210430639825271484db10caee
RaulFonseca002 <114365146+RaulFonseca002@users.noreply.github.com>
2023-03-03 01:10:20 -0300

    Add files via upload

---
+++ SomarDoisNumero.java
@@ -0,0 +1,18 @@
+package src;
+import java.util.*;
+class SomarDoisNumero {
+	public static Scanner sc =
+			new Scanner(System.in);
+	
+	//Declaracao de variaveis
+	int num1, num2, soma;
+	//Leituras
+	System.out.println("Digite um número");
+	num1 = sc.nextInt();
+	System.out.println("Digite outro número");
+	num2 = sc.nextInt();
+	//Somar
+	soma = num1 + num2;
+	//Mostrar na tela
+	System.out.println("Soma:" + soma);
+}
